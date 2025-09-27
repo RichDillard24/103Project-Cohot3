f8e0c2ffd3aca99c2eb6384edd1372372d6342f6
RichDillard24 <richiegd@hotmail.com>
2025-09-27 13:28:26 -0700

    Initial Commit

---
+++ 103Project-Cohot3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103Project-Cohot3
+//
+//  Created by Richard Dillard on 9/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103Project-Cohot3/_03Project_Cohot3App.swift
@@ -0,0 +1,17 @@
+//
+//  _03Project_Cohot3App.swift
+//  103Project-Cohot3
+//
+//  Created by Richard Dillard on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct _03Project_Cohot3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
