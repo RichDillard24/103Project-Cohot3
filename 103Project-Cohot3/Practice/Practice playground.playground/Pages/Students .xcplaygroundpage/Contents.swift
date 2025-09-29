c854f8bf7b2d98095013b21b88b556e989703c9b
RichDillard24 <richiegd@hotmail.com>
2025-09-29 16:34:59 -0700

    1st commit

--- 103Project-Cohot3/Practice/Practice playground.playground/Pages/Students .xcplaygroundpage/Contents.swift
+++ 103Project-Cohot3/Practice/Practice playground.playground/Pages/Students .xcplaygroundpage/Contents.swift
@@ -2,6 +2,31 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func calculateAverage(grades: [Double])-> Double{
+    var total: Double = 0
+    for grade in grades{
+        total += grade
+    }
+    return total/Double(grades.count)
+}
 
-//: [Next](@next)
+let studentNames: [String] = ["Alice", "Bob", "Charlie","Richad","Lindsey"]
+
+let studentGrades: [String:[Double]] = [
+    "Alice": [88.0, 90.0,80.0],
+    "Bob": [90.0, 80.0,90.0],
+    "Charlie": [70.0, 80.0,90.0],
+    "Richad": [80.0, 80.0,90.0],
+    "Lindsey": [90.0, 90.0,90.0],
+]
+
+
+
+print("Students Record \n")
+
+
+for studentName in studentNames {
+    print("Student:\(studentName)\n")
+    print("Grades: \(studentGrades[studentName]!)\n")
+    print("avg: \(calculateAverage(grades: studentGrades[studentName]!))\n")
+}
